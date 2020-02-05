227d4a4a1ec5c52a1e7ab80ab88f0f1c7eb5e14a
rachinkapoor <rachin@ost.com>
2020-02-05 16:25:12 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,93 @@
+import { AppLoading } from 'expo';
+import { Asset } from 'expo-asset';
+import * as Font from 'expo-font';
+import React, { useState } from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import {
+    OstWalletSdk,
+    OstWalletSdkEvents,
+    OstWalletWorkFlowCallback,
+    OstWalletApiError,
+    OstWalletError,
+    OstJsonApi,
+    OstWalletSdkUI,
+    OstWalletUIWorkflowCallback,
+    OstWalletSettingsDefaultConfig,
+    OstWalletSettings,
+    OstWalletSettingsComponent,
+    OstUserStatus,
+    OstDeviceStatus,
+    OstTransactionHelper
+} from '@ostdotcom/ost-wallet-sdk-react-native';
+
+console.log("OstWalletSdk",OstWalletSdk);
+console.log("OstWalletSdkEvents",OstWalletSdkEvents);
+console.log("OstWalletWorkFlowCallback",OstWalletWorkFlowCallback);
+console.log("OstWalletApiError",OstWalletApiError);
+console.log("OstWalletError",OstWalletError);
+console.log("OstJsonApi",OstJsonApi);
+console.log("OstWalletSdkUI",OstWalletSdkUI);
+console.log("OstWalletUIWorkflowCallback",OstWalletUIWorkflowCallback);
+console.log("OstWalletSettingsDefaultConfig",OstWalletSettingsDefaultConfig);
+console.log("OstWalletSettings",OstWalletSettings);
+console.log("OstWalletSettingsComponent",OstWalletSettingsComponent);
+console.log("OstUserStatus",OstUserStatus);
+console.log("OstDeviceStatus",OstDeviceStatus);
+console.log("OstTransactionHelper", OstTransactionHelper);
+
+import AppNavigator from './navigation/AppNavigator';
+
+export default function App(props) {
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+
+  if (!isLoadingComplete && !props.skipLoadingScreen) {
+    return (
+      <AppLoading
+        startAsync={loadResourcesAsync}
+        onError={handleLoadingError}
+        onFinish={() => handleFinishLoading(setLoadingComplete)}
+      />
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <AppNavigator />
+      </View>
+    );
+  }
+}
+
+async function loadResourcesAsync() {
+  await Promise.all([
+    Asset.loadAsync([
+      require('./assets/images/robot-dev.png'),
+      require('./assets/images/robot-prod.png'),
+    ]),
+    Font.loadAsync({
+      // This is the font that we are using for our tab bar
+      ...Ionicons.font,
+      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
+      // remove this if you are not using it in your app
+      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+    }),
+  ]);
+}
+
+function handleLoadingError(error) {
+  // In this case, you might want to report the error to your error reporting
+  // service, for example Sentry
+  console.warn(error);
+}
+
+function handleFinishLoading(setLoadingComplete) {
+  setLoadingComplete(true);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
